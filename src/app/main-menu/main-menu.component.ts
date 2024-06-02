472f827a160bc0fa40dd697de983b734c19efcd1
Steven Price <sprice7287@gmail.com>
2024-06-02 07:40:37 -0400

    dark theme, added search component popping in

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { SearchBookComponent } from '../search-book/search-book.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [SearchBookComponent, CommonModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
 export class MainMenuComponent {
+  showSearch: boolean = false;
+
+  toggleSearch() {
+    console.log("Toggle search is being called!")
+    this.showSearch = !this.showSearch;
+  }
+
+  toggleSearchOff(){
+    this.showSearch = false;
+  }
 
 }
