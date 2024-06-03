56d4c6d5d5cb3bd2bb63cdd57095912c5036fb66
Steven Price <sprice7287@gmail.com>
2024-06-03 12:40:05 -0400

    search functionality working, implemeneted textSearch

--- src/app/search-book/search-book.component.ts
+++ src/app/search-book/search-book.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Book, BookService } from '../services/book.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-book',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-book.component.html',
   styleUrl: './search-book.component.scss'
 })
 export class SearchBookComponent {
-  
+  query: string = "";
+  searchResults : Book[] = [];
+  constructor(private bookService: BookService){}
+
+  onSearch(query:string){
+    this.searchResults = this.bookService.searchBook(query);
+    console.log(this.searchResults)
+  }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
+export interface Book {
+  name: string;
+  author: string;
+  description: string;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
-export class BookService {
 
-  constructor() { }
+export class BookService {
+  books: Book[];
+  constructor() {
+    this.books = [
+      { name: "Apple", author: "steven", description: "book about Apples published by Steven" },
+      { name: "Orange", author: "price", description: "book about Oranges published by price" },
+      { name: "Orange + Red", author: "josh", description: "book about Orange + Red published by josh" },
+      { name: "Grapes", author: "bob", description: "book about Grapes published by bob" },
+    ]
+  }
+
+  searchBook(query: string) : Book[] {
+    let foundBooks: Book[] = [];
+
+    // text search using name and author, and description
+    foundBooks = this.books.filter(book=>
+      book.name.toLowerCase().includes(query.toLowerCase()) ||
+      book.author.toLowerCase().includes(query.toLowerCase()) ||
+      book.description.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return foundBooks;
+
+  }
+
+
+
+
 }
